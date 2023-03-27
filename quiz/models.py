3df07b828ddb3fe61fb45c182d00d77afdfed3f1
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-03-28 00:13:46 +0100

    Create UserResults model for future use

--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,3 +14,15 @@ class QuesModel(models.Model):
     
     def __str__(self):
         return self.question
+    
+class UserResults(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    result = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"{self.result} "
+            f"({self.created:%Y-%m-%d %H:%M}): "
+        )
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import QuesModel
+from .models import QuesModel, UserResults
 from django.http import HttpResponse
+
+from django.views.generic.edit import CreateView
 
 
 
@@ -31,8 +33,15 @@ def questions_list(request):
         }
         return render(request, 'quiz/results.html', context)
     else:
-        questions = QuesModel.objects.all().order_by('?')[:24] # Randomizes questions in database and selects the first 4
+        questions = QuesModel.objects.all().order_by('?')[:24] # Randomizes questions in database and selects the first 24
         return render(request, 'quiz/questions_list.html', {'questions' : questions})
 
 def results(request):
     return render(request, 'quiz/results.html')
+
+
+class SubmitResult(CreateView):
+    model = UserResults
+    result = questions_list
+    ##def get_result(request):
+    ##    if request.method == 'POST'
