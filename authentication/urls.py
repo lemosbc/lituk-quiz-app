07e9d3a9bd23cd0dcbff17fa7b2790e09163d91e
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-03-29 22:38:39 +0100

    Create registration page template

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import RegisterPage
 
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('', RegisterPage.as_view(), name='register'),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 # Create your views here.
 
-def register(request):
-    return render(request, 'authentication/register.html')
+
+class RegisterPage(FormView):
+    template_name = 'authentication/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('startquiz')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None: # If user was successfully created
+            login(self.request, user) # Login the user that was just created
+        return super(RegisterPage, self).form_valid(form)
+
+    # Redirects authencitade users if they try to access
+    # the 'register' page
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('startquiz')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+#def register(request):
+   # return render(request, 'authentication/register.html')
+
